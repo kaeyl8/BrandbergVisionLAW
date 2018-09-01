20ccbf2dce40318b0fe1080f5ba88f0e12dbb837
kaeyl <lawkaey@gmail.com>
2018-09-01 02:40:33 +0200

    hello

--- BrandbergVisionLAW/ViewController.swift
+++ BrandbergVisionLAW/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    private var branbergVision = BrandbergVision_CreateML()
+    private var hitTestResult :ARHitTestResult!
+    private var visionRequests = [VNRequest]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +29,129 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
+        /*
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        */
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        registerGestureRecognizers()
     }
+    
+    private func registerGestureRecognizers(){
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+    }
+    
+    @objc func tapped(recognizer: UIGestureRecognizer){
+        
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = self.sceneView.center
+        
+        guard let currentFrame = sceneView.session.currentFrame else {
+            return
+        }
+        
+        let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+        
+        if hitTestResults.isEmpty {
+            return
+        }
+        
+        guard let hitTestResult = hitTestResults.first else {
+            return
+        }
+        
+        self.hitTestResult = hitTestResult
+        let pixelBuffer = currentFrame.capturedImage
+        
+        performVisionRequest(pixelBuffer: pixelBuffer)
+    }
+    
+    // the function to display the prediction
+    private func displayPredictions(text :String){
+        
+        let node = createText(text: text)
+        
+        node.position = SCNVector3(self.hitTestResult.worldTransform.columns.3.x, self.hitTestResult.worldTransform.columns.3.y, self.hitTestResult.worldTransform.columns.3.z)
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
+    private func createText(text: String) -> SCNNode{
+        
+        let parentNode = SCNNode()
+        
+        let sphere = SCNSphere(radius: 0.003)
+        
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.diffuse.contents = UIColor.purple
+        sphere.firstMaterial = sphereMaterial
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        // to display the actual title of the subject
+        let textGeometry = SCNText(string: text, extrusionDepth: 0)
+        /*
+        textGeometry.alignmentMode = kCAAlignmentCenter
+        */
+        textGeometry.alignmentMode = CATextLayerAlignmentMode.center.rawValue
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        textGeometry.firstMaterial?.specular.contents = UIColor.white
+        textGeometry.firstMaterial?.isDoubleSided = true
+        
+        let font = UIFont(name: "Marion-Bold", size: 0.15)
+        textGeometry.font = font
+        
+        let textNode = SCNNode(geometry: textGeometry)
+        textNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        
+        parentNode.addChildNode(sphereNode)
+        parentNode.addChildNode(textNode)
+        return parentNode
+        
+    }
+    
+    private func performVisionRequest(pixelBuffer :CVPixelBuffer){
+        
+        let visionModel = try! VNCoreMLModel(for: self.branbergVision.model)
+        
+        let request = VNCoreMLRequest(model: visionModel){ request, error in
+            if error != nil{
+                return
+            }
+            
+            guard let observations = request.results else {
+                return
+            }
+            
+            let observation = observations.first as! VNClassificationObservation
+            
+            print("Name \(observation.identifier) and confidence is \(observation.confidence)")
+            
+            DispatchQueue.main.async {
+                self.displayPredictions(text: observation.identifier)
+            }
+            
+        }
+        
+        request.imageCropAndScaleOption = .centerCrop
+        
+        self.visionRequests = [request]
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: [:])
+        
+        DispatchQueue.global().async {
+            try! imageRequestHandler.perform(self.visionRequests)
+        }
+        
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
